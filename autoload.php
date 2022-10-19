82e88777488a03429a7c00d02ebea02e1c3d4ea0
Bryan <bryanferrando59@gmail.com>
2022-10-19 18:31:37 +0200

    Changes

--- autoload.php
+++ autoload.php
@@ -4,10 +4,13 @@ class AutoLoader {
   
   public static function register() {
     spl_autoload_register(function ($class) {
-      $search = ['Controller', 'Service', 'Config'];
+      echo "class: $class ";
+      $pattern = ['/Controller\b/', '/Service\b/', '/Config\b/'];
       $replace = ['.controller', '.service', '.config'];
       
-      $file = strtolower(str_replace($search, $replace, $class) . '.php');
+      $file = strtolower(preg_replace($pattern, $replace, $class) . '.php');
+      
+      $test = preg_replace($pattern, $replace, 'Controller');
       
       if(file_exists($file)){
         return require_once $file;
