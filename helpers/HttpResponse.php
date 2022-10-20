6084db6f0bbcc75f98d53c2a564f3c8cd9b40fe7
BryanFRD <bryanferrando59@gmail.com>
2022-10-20 11:35:44 +0200

    Changes

---
+++ helpers/HttpResponse.php
@@ -0,0 +1,33 @@
+<?php namespace Helpders;
+
+class HttpResponse {
+  
+  /**
+   * Cette méthode fixe le status code de la réponse HTTP,
+   * si le status est >= 300 elle appelle la méthode exit
+   * Cette méthode écrit dans le flux de sortie les data au format json,
+   * puis elle arrête l'exécution du script
+   */
+  public static function send(array $data, int $status = 200) : void {
+    http_response_code($status);
+    
+    if($status >= 300){
+      return exit($status);
+    }
+    
+    echo json_encode($data);
+  }
+  
+  /**
+   * Cette méthode fixe le status code de la réponse HTTP (>= 300)
+   * puis elle arrête l'exécution du script
+   */
+  public static function exit(int $status = 404) : void {
+    http_response_code($status);
+    
+    die;
+  }
+  
+}
+
+?>
