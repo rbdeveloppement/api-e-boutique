e9e1e36483d320b786d61a34dae1a1b3a98cf041
Bryan <bryanferrando59@gmail.com>
2022-11-04 09:48:47 +0100

    Changes

--- src/services/Database.service.php
+++ src/services/Database.service.php
@@ -117,10 +117,9 @@ class DatabaseService {
             $values .= "?, ";
             array_push($valuesToBind, $v);
         }
-        $values = substr($values, 0, -2) . "), ";
+        $values = substr($values, 0, -2) . ")";
     }
     
-    $columns = substr($columns, 0, -2);
     $values = substr($values, 0, -2);
     
     $sql = "INSERT INTO $this->table $columns VALUES $values ON DUPLICATE KEY UPDATE $duplicateUpdate;";
